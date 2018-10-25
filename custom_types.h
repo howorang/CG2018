638c44403cd4963493f95f52b5d6326ad389e567
Piotr Borczyk <howorang@gmail.com>
2018-10-26 00:52:20 +0200

    Changes and stuff

---
+++ custom_types.h
@@ -0,0 +1,14 @@
+//
+// Created by Piotr Borczyk on 16.10.2018.
+//
+
+#ifndef EX1_CUSTOM_TYPES_H
+#define EX1_CUSTOM_TYPES_H
+#include <GL/gl.h>
+#include <GL/glut.h>
+
+typedef struct Point2d {
+    GLfloat x;
+    GLfloat y;
+} Point2d;
+#endif //EX1_CUSTOM_TYPES_H
--- main.cpp
+++ main.cpp
@@ -2,92 +2,118 @@
 #include <windows.h>
 #include <GL/gl.h>
 #include <GL/glut.h>
+#include "custom_types.h"
 #include <stdlib.h>
+void triangle(GLfloat red, GLfloat green, GLfloat blue);
+
+void timer(int);
+
+void rotateByCenterOfMass(GLfloat ax,  GLfloat ay,
+                          GLfloat bx, GLfloat by,
+                          GLfloat cx, GLfloat cy);
+
+void drawTriangle(GLfloat x, GLfloat y,
+                  GLfloat red, GLfloat green, GLfloat blue);
+
+void drawQuarter();
+
+float degreee = 0;
+
 void MyDisplay(void) {
-    // Wyswietlana scena - poczatek
-    glLoadIdentity();
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();//=1
+    // The new scene
+    glRotated(degreee, 0, 0, 1);
+    drawQuarter();
+
+    glTranslated(-(100/3)*2,0,0);
+    glRotated(90, 0, 0, 1);
+    drawQuarter();
+
+    glTranslated(-(100/3)*2,0,0);
+    glRotated(90, 0, 0, 1);
+    drawQuarter();
+
+    glTranslated(-(100/3)*2,0,0);
+    glRotated(90, 0, 0, 1);
+    drawQuarter();
+    // The end of scene
+    glFlush();//start processing buffered OpenGL
+    glutTimerFunc(5, timer, 0);
+    glutSwapBuffers();
+}
+
+void drawQuarter() {
+    drawTriangle(0, 0,
+                 0, 0, 255);
+    drawTriangle(0,100,
+                 0,255,0);
+    drawTriangle(100,100,
+                 255,255,0);
+    drawTriangle(0,200,
+                 255,0,0);
+
+
+    drawTriangle(100,0,
+                 207,47, 47);
+    drawTriangle(200,0,
+                 255,0,144);
+}
+
+void drawTriangle(GLfloat x, GLfloat y,
+                  GLfloat red, GLfloat green, GLfloat blue) {
+    glPushMatrix();
+    glTranslated(x, y, 0);
+    rotateByCenterOfMass(100, 0,
+                         0, 100,
+                         0, 0);
+    triangle(red, green, blue);
+    glPopMatrix();
+}
+
+void rotateByCenterOfMass(GLfloat ax,  GLfloat ay,
+        GLfloat bx, GLfloat by,
+        GLfloat cx, GLfloat cy) {
+    GLfloat xc = (ax + bx + cx) / 3;
+    GLfloat yc = (ay + by + cy) / 3;
+    glRotated(-(degreee * 3), 0, 0, 1);
+    glTranslated(-xc, -yc, 0);
+}
+
+void timer(int par) {
+  // degreee += 0.25;
+    if (degreee == 360) {
+        degreee = 0;
+    }
+    glutPostRedisplay();
+}
+
+void triangle(GLfloat red, GLfloat green, GLfloat blue) {
     glBegin(GL_POLYGON);
-    //zmiana stanu OpenGL’a
-    glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-    //Red;
-    glVertex2f(100.0f, 50.0f);
-    glColor4f(0.0f, 1.0f, 0.0f, 1.0f);
-    //Green
-    glVertex2f(450.0f, 400.0f);
-    glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
-    //Blue
-    glVertex2f(450.0f, 50.0f);
+    glColor4f(red, green, blue, 1.0f);
+    glVertex2f(100.0f, 0.0f);
+    glVertex2f(0.0f, 0.0f);
+    glVertex2f(0.0f, 100.0f);
     glEnd();
-    // Wyswietlana scena - koniec
-    glFlush();
-    //start processing buffered OpenGL routines
 }
 void MyInit(void) {
-    glClearColor(0.0, 0.0, 0.0, 0.0);
-    //select clearing (background) color
+    glClearColor(0.0, 0.0, 0.0, 0.0);//select clearing (background) color
     /* initialize viewing values */
-    glViewport(0, 0, 300, 300);
-    //pocz•tek u.ws. lewy górny róg
-    glMatrixMode(GL_PROJECTION
-    );//Nast•pne 2 wiersze b•d• modyfikowały m. PROJECTION
-    glLoadIdentity();
-    //inicjalizacja
-    gluOrtho2D(0.0, 500.0*1.2, 0.0, 500.0);
+    glViewport(0, 0, 300, 300);//window origin and size
+    glMatrixMode(GL_PROJECTION);//
+    gluOrtho2D(-300.0, 300.0, -300.0, 300.0);
     glMatrixMode(GL_MODELVIEW);
-    //Nast•pny wiersz b•dzie modyfikował m. MODELVIEW
-    glLoadIdentity();
+    glLoadIdentity();//=1
 }
-/*int APIENTRY WinMain(HINSTANCE hInstance,
-HINSTANCE hPrevInstance,
-LPSTR     lpCmdLine,
-int       nCmdShow){
-glutInit(&__argc, __argv);             */
-int main(int argc, char** argv) {
+
+int main(int argc, char** argv) { //<- for normal API
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    //single buffer and RGBA
-    glutInitWindowSize(250, 250);
-    //initial window size
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);//single buffer and RGBA
+    glutInitWindowSize(600, 600);//initial window size
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("My window");
-    //create widnow, hello title bar
+    glutCreateWindow("My window");//create widnow, hello title bar
     MyInit();
-    glutDisplayFunc(MyDisplay);
-    //register display function (call-back)
-    /*     glutSetMenu(menu);
-    glutReshapeFunc(NULL);
-    glutReshapeFunc(NULL);
-    glutKeyboardFunc(NULL);
-    glutKeyboardFunc(NULL);
-    glutMouseFunc(NULL);
-    glutMouseFunc(NULL);
-    glutMotionFunc(NULL);
-    glutMotionFunc(NULL);
-    glutVisibilityFunc(NULL);
-    glutVisibilityFunc(NULL);
-    glutMenuStateFunc(NULL);
-    glutMenuStateFunc(NULL);
-    glutMenuStatusFunc(NULL);
-    glutMenuStatusFunc(NULL);
-    glutSpecialFunc(NULL);
-    glutSpecialFunc(NULL);
-    glutSpaceballMotionFunc(NULL);
-    glutSpaceballMotionFunc(NULL);
-    glutSpaceballRotateFunc(NULL);
-    glutSpaceballRotateFunc(NULL);
-    glutSpaceballButtonFunc(NULL);
-    glutSpaceballButtonFunc(NULL);
-    glutButtonBoxFunc(NULL);
-    glutButtonBoxFunc(NULL);
-    glutDialsFunc(NULL);
-    glutDialsFunc(NULL);
-    glutTabletMotionFunc(NULL);
-    glutTabletMotionFunc(NULL);
-    glutTabletButtonFunc(NULL);
-    glutTabletButtonFunc(NULL);
-    glutTimerFunc(100, NULL, 1); ... */
-    glutMainLoop();
-    //enter main loop and process events
+    glutDisplayFunc(MyDisplay);//
+    glutMainLoop();//enter main loop and process events
     return 0;
 }
