367e1d78a4ddf527b44d673d85a2369a610b34d0
howorang <howorang@gmail.com>
2018-12-30 20:19:53 +0100

    Planets

--- Ex3/main.cpp
+++ Ex3/main.cpp
@@ -10,25 +10,101 @@
 #include "Camera.h"
 #include <SOIL.h>
 
+typedef struct PlanetConf {
+    GLuint texture;
+    GLfloat size;
+    GLfloat semiMajorAxis;
+    GLfloat eccentricity;
+    GLfloat orbitalPeriod;
+    GLfloat dayLength;
+} PlanetConf;
+
+static const int TIME_FACTOR = 50;
+static const int SCALE_FACTOR = 250;
 Camera *camera = nullptr;
+
+GLuint sunTexture;
+PlanetConf planetConfs[8];
 
 double time = 0;
 void timer(int);
 
-void init (void) {
+void drawSun();
 
+void drawPlanet(PlanetConf* planetConf);
+
+void drawPlanetSimple(GLuint texture, GLfloat size);
+
+GLuint loadTexture(const char *texturePath);
+
+void init (void) {
+    sunTexture = loadTexture("assets/sun.jpg");
+
+    PlanetConf mercuryConf = {.texture = loadTexture("assets/mercury.jpg"), .size = 2439,
+                              .semiMajorAxis = 0.3870, .eccentricity = 0.2056, .orbitalPeriod = 0.2408, .dayLength = 1};
+
+    PlanetConf venusConf = {.texture = loadTexture("assets/venus.jpg"), .size = 6051,
+            .semiMajorAxis = 0.7233, .eccentricity = 0.0067, .orbitalPeriod = 0.6151, .dayLength = 1};
+
+    PlanetConf earthConf = {.texture = loadTexture("assets/earth.jpg"), .size = 6378,
+            .semiMajorAxis = 1, .eccentricity = 0.01671, .orbitalPeriod = 1, .dayLength = 1};
+
+    PlanetConf marsConf = {.texture = loadTexture("assets/mars.jpg"), .size = 3396,
+            .semiMajorAxis = 1.5237, .eccentricity = 0.09339, .orbitalPeriod = 1.8808, .dayLength = 1};
+
+    PlanetConf jupiterConf = {.texture = loadTexture("assets/jupiter.jpg"), .size = 71492,
+            .semiMajorAxis = 5.2029, .eccentricity = 0.0484, .orbitalPeriod = 11.8626, .dayLength = 1};
+
+    PlanetConf saturnConf = {.texture = loadTexture("assets/saturn.jpg"), .size = 60268,
+            .semiMajorAxis = 9.537, .eccentricity = 0.0539, .orbitalPeriod = 29.4474, .dayLength = 1};
+
+    PlanetConf uranusConf = {.texture = loadTexture("assets/uranus.jpg"), .size = 25559,
+            .semiMajorAxis = 19.189, .eccentricity = 0.04726, .orbitalPeriod = 84.0168, .dayLength = 1};
+
+    PlanetConf neptuneConf = {.texture = loadTexture("assets/neptune.jpg"), .size = 24764,
+            .semiMajorAxis = 30.0699, .eccentricity = 0.00859, .orbitalPeriod = 164.7913, .dayLength = 1};
+
+    planetConfs[0] = mercuryConf;
+    planetConfs[1] = venusConf;
+    planetConfs[2] = earthConf;
+    planetConfs[3] = marsConf;
+    planetConfs[4] = jupiterConf;
+    planetConfs[5] = saturnConf;
+    planetConfs[6] = uranusConf;
+    planetConfs[7] = neptuneConf;
+}
+
+GLuint loadTexture(const char *texturepath) {
+    return SOIL_load_OGL_texture
+            (
+                    texturepath,
+                    SOIL_LOAD_AUTO,
+                    SOIL_CREATE_NEW_ID,
+                    SOIL_FLAG_MIPMAPS | SOIL_FLAG_NTSC_SAFE_RGB | SOIL_FLAG_COMPRESS_TO_DXT
+            );
 }
 
 void enable (void) {
     glEnable (GL_DEPTH_TEST); //enable the depth testing
-   // glEnable (GL_LIGHTING); //enable the lighting
-    //glEnable (GL_LIGHT0); //enable LIGHT0, our Diffuse Light
+  //  glEnable (GL_LIGHTING); //enable the lighting
+  //  glEnable (GL_LIGHT0); //enable LIGHT0, our Diffuse Light
     glShadeModel (GL_SMOOTH); //set the shader to smooth shader
     glEnable(GL_TEXTURE_2D);
 
 }
 
-
+void drawPlanet(PlanetConf* planetConf) {
+    GLfloat a = planetConf -> semiMajorAxis;
+    GLfloat c = a * planetConf -> eccentricity;
+    GLfloat b  = (GLfloat)(sqrt(pow(a,2) - pow(c,2)));
+    GLfloat posx = (GLfloat)(a * cos(time/ TIME_FACTOR /planetConf -> orbitalPeriod));
+    GLfloat posy = (GLfloat)(b * sin(time / TIME_FACTOR / planetConf -> orbitalPeriod));
+    glPushMatrix();
+    glTranslatef(posx * SCALE_FACTOR, 0, posy * SCALE_FACTOR);
+    glTranslatef(0, 0, planetConf -> eccentricity * SCALE_FACTOR);
+    drawPlanetSimple(planetConf -> texture, planetConf->size);
+    glPopMatrix();
+}
 
 void display (void) {
     glClearColor (0.0,0.0,0.0,1.0); //clear the screen to black
@@ -36,11 +112,38 @@ void display (void) {
     glLoadIdentity();
     camera->camera();
     enable();
-    //
+    drawSun();
+    for (int i = 0; i < 8; ++i) {
+        drawPlanet(&planetConfs[i]);
+    }
     glutTimerFunc(5, timer, 0);
     glFlush();
     glutSwapBuffers(); //swap the buffers
 }
+
+void drawPlanetSimple(GLuint texture, GLfloat size) {
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    GLUquadricObj *sphereQuadratic = gluNewQuadric();
+    gluQuadricDrawStyle(sphereQuadratic, GLU_FILL);
+    gluQuadricTexture(sphereQuadratic, TRUE);
+    gluSphere(sphereQuadratic, size/1000, 50, 50);
+    gluDeleteQuadric(sphereQuadratic);
+}
+
+void drawSun() {
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glBindTexture(GL_TEXTURE_2D, sunTexture);
+    GLUquadricObj* sunQuadratic = gluNewQuadric();
+    gluQuadricDrawStyle(sunQuadratic, GLU_FILL);
+    gluQuadricTexture(sunQuadratic, TRUE);
+    gluSphere(sunQuadratic, 695700/20000, 50, 50);
+    gluDeleteQuadric(sunQuadratic);
+}
+
+
 
 
 void reshape (int w, int h) {
